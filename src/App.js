e6aa416041268d6fc5efbd2ba10fb627d5824394
YanGraim <yangraim@gmail.com>
2024-12-12 14:53:56 -0300

    Utiliza UseState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
 import Nome from './components/Nome';
 
 function App() {
+  const [aluno, setAluno] = useState('Yan Graim')
+  function handleChangeName(nome) {
+    setAluno(nome);
+  }
+
   return (
     <div>
-      <h1>Bem vindo ao meu projeto</h1>
-      <Nome aluno = "Yan Graim" idade = {27}/>
-      <br/>
-      <br/> 
-      <Nome aluno = "Wanessa Niederauer" idade = {23}/>
+      <h1>Bem vindo ao meu projeto</h1> <br/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={ () => handleChangeName('Wanessa Niederauer')}>
+        Mudar nome
+      </button>
     </div>
   );
 }
