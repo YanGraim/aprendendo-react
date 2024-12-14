2ba794256293bf983963b062e78f290efbb62734
YanGraim <yangraim@gmail.com>
2024-12-14 12:17:53 -0300

    listas com useState([])

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import { useState } from 'react';
 
 function App() {
-  const [nome, setNome] = useState('');
-  const [idade, setIdade] = useState('');
-  const [email, setEmail] = useState('');
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar conta de luz',
+    'Estudar React JS'
+  ]);
 
-  const [user, setUser] = useState({});
 
   function handleRegister(e) {
     e.preventDefault();
 
-    alert('Usuario registrado!');
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email
-    })
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
@@ -25,14 +21,10 @@ function App() {
 
       <form onSubmit={handleRegister}>
         <label>Nome:</label><br/>
-        <input placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)}/><br/>
-
-        <label>Email:</label><br/>
-        <input placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/><br/>
-
-        <label>Idade:</label><br/>
-        <input placeholder="Digite sua idade" value={idade} onChange={(e) => setIdade(e.target.value)}/><br/>
-        
+        <input 
+          placeholder="Digite uma tarefa" 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)}/>
         <br/>
         <button type='submit'>
           Registar
@@ -41,11 +33,11 @@ function App() {
 
       <br/> <br/>
 
-      <div>
-        <span>Bem vindo: {user.nome}</span> <br/>
-        <span>Idade: {user.idade}</span> <br/>
-        <span>Email: {user.email}</span> <br/>
-      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li> // o react sempre pede para passar uma key quando se utiliza uma lista
+        ))}
+      </ul>
     </div>
   );
 }
